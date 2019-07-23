0c93e926fac51c5bb9c01fb3095e3a009380594b
BrettGallagher <brett@techfire225.org>
2019-07-23 17:02:50 -0400

    message deletion website added

--- TechFireBot.py
+++ TechFireBot.py
@@ -1,51 +1,51 @@
 import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
 import json
 import asyncio
 import time
-
 from profanity import profanity
-
+import aiotba
+import datetime
+from random import *
+current = datetime.datetime.now()
 Client = discord.Client()
 client = commands.Bot(command_prefix="^")
-custom_badwords = ["douche", "bitch", "asshole", "ass", "swerve"] #makes custom profanity filter triggers
-profanity.load_words(custom_badwords)
+
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+gpquote = sample(number, 1)
+if number > [5]:
+    gpquote = "Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence"
+elif number < [5]:
+    gpquote = "ok"
+
+
 @client.event
-async def on_ready(): #initialization
+async def on_ready():
     print(client.user.name)
     print("is Setup!")
-    await client.change_presence(game=discord.Game(name="Getting ready for the 2019 season!", type=3))
-
-def remove_formatting(text):
-    return ''.join([x.lower() for x in text if ord(x) != 42 and ord(x) != 95 and ord(x) != 126 and ord(x) != 96]) #makes all forms of text able to be understood by bot
+    await client.change_presence(game=discord.Game(name="Preparing for IRI!", type=3))
 
 @client.event
-async def on_message(message):
-        if remove_formatting(message.content) == "what events are we going to?":
-            await client.send_message(message.channel, "We are going to Springside Chestnut Hill and Montgomery!")
-        elif remove_formatting(message.content) == "We are going to Springside Chestnut Hill and Montgomery!":
-            await client.send_message(message.channel, "We are going to Springside Chestnut Hill and Montgomery!")
+async def on_message():
+    global gpquote
+    #number = random.randint(1, 10)
+    #if number == 1:
+       # gpquote = "Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence"
 
 
-@client.event #profanity filter
+@client.event
 async def on_message(message):
 
     if profanity.contains_profanity(message.content):
         await client.delete_message(message)
-        await client.send_message(message.channel, "That is not GP!")
-        print("Bad word removed")
+        await client.send_message(message.channel, gpquote)
+
+@client.event
+async def on_message_delete(message):
+    deldoc = open("deleted messages.txt", "a",)
+    await deldoc.write(message.content + ";" + str(current) +"|")
 
 
 
 
-
-
-
-
-
-
-
-
-
-client.run("Don't steal my Token please")
+client.run("Place Token Here")
